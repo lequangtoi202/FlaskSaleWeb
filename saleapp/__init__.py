85299dffb7ff9f7703471c4967c790bcca7f0734
unknown <2051052140toi@ou.edu.vn>
2022-10-27 19:56:43 +0700

    mysql + sqlalchemy

--- saleapp/__init__.py
+++ saleapp/__init__.py
@@ -1,3 +1,8 @@
 from flask import Flask
+from urllib.parse import quote
+from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql+pymysql://root:%s@localhost/saleapp?charset=utf8mb4' % quote('lqt25092002')
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = True
+db = SQLAlchemy(app=app)
--- saleapp/index.py
+++ saleapp/index.py
@@ -6,9 +6,16 @@ from saleapp import app, dao
 @app.route("/")
 def index():
     categories = dao.load_categories()
-    products = dao.load_products(category_id=request.args.get('category_id'))
+    products = dao.load_products(category_id=request.args.get('category_id'), kw=request.args.get('kw'))
     return render_template("index.html", categories=categories, products=products)
+
+
+@app.route('/products/<int:product_id>')
+def details(product_id):
+    p = dao.get_product_by_id(product_id)
+    return render_template("details.html", product=p)
 
 
 if __name__ == '__main__':
     app.run(debug=True)
+
